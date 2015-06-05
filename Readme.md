586752a2e8bb083884abc01aa76faf6263a0e193
Nzen <nmprado@csupomona.edu>
2015-06-04 21:12:10 -0500

    Bislicing ready
    Bislice now has a depth first search algorithm, which I'll put into
    colorspace. Part of my problem was python's default for [].pop() is to pop
    the last when it has no argument. Well, I'm lucky it can pop from
    anywhere.
    
    Next is clearly to put this in colorspace, but I don't doubt I'll find
    distractions in the other classes.

--- Readme.md
+++ Readme.md
@@ -3,16 +3,16 @@
 
 This is a little text filter that puts color-styled spans around nonterminal words within &lt;code&gt; tags. Eventually intended as a plugin for PDistillery. Also useful as a standalone library to color your self-hosted code in html.
 
-This currently has _prototypical_ behavior. The color is set by sine waves along RGB, the spans go around every three characters, and so on. _Eventually_, I intend to walk the LAB colorspace to color the variables in a couple languages I use.
+This currently has _prototypical_ behavior. The color is set by sine waves along RGB, the spans only ignore whitespace and math chars, and so on. _Eventually_, I intend to walk the LAB colorspace to color the variables in a couple languages I use.
 
-MIT License 2015, as described at [choosealicense.com/licenses/mit/]
+MIT License 2015, as described at [choosealicense.com](http://choosealicense.com/licenses/mit/)
 
-### Todo (14 May)
+### Todo (29 May)
 
-* LitmusFrame: build transmix from a file
-* Litmus: break the input into non/code blocks
-* Transmix: take a Path object instead of a filepath
+* LitmusFrame: build a legit transmix, alter the path to determine where output goes
+* Litmus: process code blocks concurrently?, audit api for plugin use
+* Transmix: require a Path object instead of a filepath
 * Colorspace: bislice the rgb colorspace
-* LangLexer: use a hard regex that avoids keywords/operators
-* SyntaxPDautoma: diagnose why it's not marking anything as terminal
+* LangLexer: take a config collection of operators and keywords for arbitrary langs
+* SyntaxPDautoma: ignore multichar operators, ignore numbers
 * (all): tests for the various pieces
--- bislice.py
+++ bislice.py
@@ -1,121 +1,129 @@
 
 class qN(object) :
-	def __init__(self, low_lim, difference) :
+	def __init__(self, low_lim, upper_bound) :
 		self.start = low_lim
-		self.dist = difference
+		self.fin = upper_bound
 	def pr( self ) :
-		print "s%d-d%d  " % ( self.start, self.dist ),
+		print "s%d-d%d " % ( self.start, self.fin ),
 
 class queue( object ) :
 	def __init__( self, firstNode ) :
 		self.ll = [ firstNode ]
 	def gFirst( self ) :
-		return self.ll.pop()
+		return self.ll.pop(0) # wow. no arg is last, not first
 	def sLast(  self, node ) :
 		self.ll.append( node )
 	def containsAnElement( self ) :
 		return len(self.ll)
 	def pr(self) :
-		print "::-",
+		print "  ::-",
 		for nn in self.ll :
 			nn.pr()
 		print
 
-class result( object ) :
-	def __init__( self, equals, qq ) :
-		self.answer = equals
-		self.que = qq
-
-def next( queu ) :
-	answer = 0
-	temp = queu.gFirst()
-	print "n t.s %d t.d %d" % ( temp.start, temp.dist )
-	if temp.dist < 1 :
-		answer = temp.start
-		print "- ans %d" % ( answer ) # 4TESTS
+# calculates one at a time
+def mandolin( qu ) :
+	# I'll assume client checks qu.containsAnElement() :
+	#qu.pr() # 4TESTS
+	intermed = qu.gFirst()
+	if intermed.start >= intermed.fin :
+		return intermed.fin, qu
+	sta = intermed.start
+	end = intermed.fin
+	dist = end - sta
+	if dist is 1 :
+		qu.sLast( qN(sta, sta) )
+		return end, qu
+	if dist is 2 :
+		qu.sLast( qN(sta, sta) )
+		qu.sLast( qN(end, end) )
+		return sta +1, qu # mid
 	else :
-		answer = temp.dist / 2 + temp.start
-		print ") ans %d" % ( answer ) # 4TESTS
-		
-		fin = temp.start +temp.dist -1
-		dist_L = answer -temp.start -1
-		dist_R = fin -answer -1
-		
-		#print "L sl.s %d sl.d %d" % ( temp.start, dist_L ) # 4TESTS
-		queu.sLast(qN( temp.start, dist_L ))
-		
-		#print "R sl.s %d sl.d %d" % ( answer +1, dist_R ) # 4TESTS
-		queu.sLast(qN( answer +1, dist_R ))
-	return result( answer, queu )
+		mid = dist / 2 + sta
+		qu.sLast( qN(mid +1, end) )
+		qu.sLast( qN(sta, mid -1) )
+		return mid, qu
 
+# NOTE exponential precalculation and storage, not good in my use case
 def slice(start, end) :
-	qu = queue(qN( start, end -1 ))
+	qu = queue( qN(start, end) )
 	while qu.containsAnElement() :
-		intermed = next( qu )
-		#print ">%d" % (intermed.answer), #4real, but I'm already printing everything else
-		qu = intermed.que
-		qu.pr()
-		raw_input()
+		qu.pr() # 4TESTS
+		intermed = qu.gFirst()
+		if intermed.start >= intermed.fin :
+			print "> %d\n" % ( intermed.start )
+			continue # else
+		st = intermed.start
+		en = intermed.fin
+		dist = en - st
+		if dist is 1 :
+			#print "\tst %d en %d" % (st, en)
+			qu.sLast( qN(st, st) )
+			qu.sLast( qN(en, en) )
+		if dist is 2 :
+			#print "\tst %d mid %d en %d" % (st, st +1, en)
+			qu.sLast( qN(st +1, st +1) ) # mid
+			qu.sLast( qN(st, st) )
+			qu.sLast( qN(en, en) )
+		else :
+			mid = dist / 2 + st
+			#print "\tst %d mid %d en %d" % (st, mid, en)
+			qu.sLast( qN(mid, mid) )
+			qu.sLast( qN(mid +1, en) )
+			qu.sLast( qN(st, mid -1) )
 
-ending = 10
-print "begin s1,e%d" % (ending)
-slice(1, ending)
+def run_basic() :
+	starter = 1
+	ending = 15
+	print "begin s1,e%d" % (ending)
+	# slice(starter, ending)
+	val = -1
+	ku = queue( qN(starter, ending) )
+	while ku.containsAnElement() :
+		val,ku = mandolin(ku)
+		raw_input( val )
+
+# cause they make triple blades :B
+def gilette() :
+	low = 0
+	hi = 255
+	red = queue( qN(low, hi) )
+	lue = queue( qN(low, hi) )
+	een = queue( qN(low, hi) )
+	rr = low
+	bb = low
+	gr = low
+	while red.containsAnElement() :
+		rr,red = mandolin(red)
+		bb,lue = mandolin(lue)
+		gr,een = mandolin(een)
+		raw_input( str(rr) +" "+ str(bb) +" "+ str(gr) )
+
+# specify here, as an inner class or object fields
+def gap( val ) :
+	if val <= 7 or val >= 12 :
+		return val
+	else :
+		return -1
+
+# this is how I'll avoid the background color
+def chinked_blade() :
+	starter = 1
+	ending = 15
+	print "begin s1,e%d" % (ending)
+	# slice(starter, ending)
+	val = -1
+	ku = queue( qN(starter, ending) )
+	while ku.containsAnElement() :
+		val,ku = mandolin(ku)
+		if gap( val ) < 0 :
+			continue
+		raw_input( val )
+
+#run_basic()
+#gilette()
+chinked_blade()
 """
-fn(qu)
-  answer;
-  temp = qu.gFirst()
-  if temp.dist < 1
-	answer = qu.start
-  else
-    answer = temp.dist/2 + start // may be off and needs floor
-    qu.sLast( new qN(temp.start, temp.dist-answer) ) // FIX
-	qu.sLast( new qN(answer +1, temp.start + dist - answer) ) // CHECK
-  return new result( answer, qu )
-
-bislice( start, end )
-  new qu( new qN(start, end -start) )
-  while qu.containsAnElement()
-    intermed = fn(qu)
-	print intermed.answer
-	qu = intermed.q
-
- * you may ask yourself, why distance? I say, my space is so big,
-	I don't want to hold the leaves or full graph, like a bst would
-	which is a bit of a cheat, given that N^ is my 'graph'
-
-====
-	CURRENT
-
-begin s1,e10
-n t.s 1 t.d 9
-) ans 5
-::- s1-d3   s6-d3
-
-n t.s 6 t.d 3
-) ans 7
-::- s1-d3   s6-d0   s8-d0
-
-n t.s 8 t.d 0
-- ans 8
-::- s1-d3   s6-d0
-
-n t.s 6 t.d 0
-- ans 6
-::- s1-d3
-
-n t.s 1 t.d 3
-) ans 2
-::- s1-d0   s3-d0
-
-n t.s 3 t.d 0
-- ans 3
-::- s1-d0
-
-n t.s 1 t.d 0
-- ans 1
-::-
---
-
   breadth first search
 1  procedure BFS(G,v) is
 2      let Q be a queue
@@ -129,10 +137,4 @@ n t.s 1 t.d 0
 10                label w as discovered
 
 The non-recursive implementation is similar to depth-first search but differs from it in two ways: it uses a queue instead of a stack, and it checks whether a vertex has been discovered before enqueueing the vertex rather than delaying this check until the vertex is dequeued from the queue.
-
-;;
-In order to print the binary tree in level order with newline in the end of each level, we can utilize two queues. The first queue stores the current level's nodes, while the second queue stores the next level's nodes (the current level nodes' children).
-,
-Is it possible that a solution exists using only one single queue? Yes, you bet. The single queue solution requires two extra counting variables which keep tracks of the number of nodes in the current level (nodesInCurrentLevel) and the next level (nodesInNextLevel). When we pop a node off the queue, we decrement nodesInCurrentLevel by one. When we push its child nodes to the queue, we increment nodesInNextLevel by two. When nodesInCurrentLevel reaches 0, we know that the current level has ended, therefore we print an endline here.
-
 """
